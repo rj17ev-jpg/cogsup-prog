bee0ba7ec28f13ade3f220e701d55aec42afb6ab
riya jain <rj17ev@gmail.com>
2025-10-03 17:42:13 +0200

    drawing functions

--- Week-4/Exercises/drawing_functions.py
+++ Week-4/Exercises/drawing_functions.py
@@ -2,14 +2,20 @@ from expyriment import design, control, stimuli
 import random
 
 def load(stims):
-    pass
+    for i in stims:
+        i.preload()
 
 def timed_draw(stims):
-    pass
+    time = []
+    for i, stim in enumerate(stims):
+        t = stim.present(clear=(i == 0), update=(i == len(stims) - 1))
+        time.append(t)
+    return sum(time)
     # return the time it took to draw
 
 def present_for(stims, t=1000):
-    pass
+    dt = timed_draw(stims)
+    exp.clock.wait(t - dt)
 
 
 """ Test functions """
@@ -21,7 +27,7 @@ control.initialize(exp)
 fixation = stimuli.FixCross()
 load([fixation])
 
-n = 20
+n = 5
 positions = [(random.randint(-300, 300), random.randint(-300, 300)) for _ in range(n)]
 squares = [stimuli.Rectangle(size=(50, 50), position = pos) for pos in positions]
 load(squares)
@@ -31,9 +37,9 @@ durations = []
 t0 = exp.clock.time
 for square in squares:
     if not square.is_preloaded:
-        print("Preloading function not implemneted correctly.")
+        print("Preloading function not implemented correctly.")
     stims = [fixation, square] 
-    present_for(stims, 500)
+    present_for(stims, t=500)
     t1 = exp.clock.time
     durations.append(t1-t0)
     t0 = t1
